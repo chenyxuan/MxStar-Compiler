cedc792089a3454042882e46771ba8588936c6c2
lgkm39 <209985587@qq.com>
2019-05-12 17:32:17 +0800

    update allocator and fix lib

---
+++ lib/output.asm
@@ -0,0 +1,234 @@
+
+
+
+
+
+
+default rel
+
+global main
+global ans
+
+extern printf
+extern _GLOBAL_OFFSET_TABLE_
+
+
+SECTION .text   
+
+main:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     dword [rbp-4H], 328
+        mov     dword [rbp-8H], 0
+L_001:  mov     eax, dword [rbp-8H]
+        cmp     eax, dword [rbp-4H]
+        jge     L_002
+        mov     eax, dword [rbp-8H]
+        cdqe
+        lea     rdx, [rax*4]
+        lea     rax, [rel ans]
+        mov     eax, dword [rdx+rax]
+        mov     esi, eax
+        lea     rdi, [rel L_003]
+        mov     eax, 0
+        call    printf
+        add     dword [rbp-8H], 1
+        jmp     L_001
+
+L_002:  mov     eax, 0
+        leave
+        ret
+
+
+
+SECTION .data   align=32
+
+ans:
+        db 2AH, 0F7H, 0FFH, 0FFH, 2AH, 0F7H, 0FFH, 0FFH
+        db 6CH, 04H, 00H, 00H, 0A4H, 03H, 00H, 00H
+        db 0F8H, 03H, 00H, 00H, 0C3H, 0FH, 00H, 00H
+        db 5BH, 0AH, 00H, 00H, 76H, 19H, 00H, 00H
+        db 70H, 0FH, 00H, 00H, 37H, 05H, 00H, 00H
+        db 0FFH, 05H, 00H, 00H, 2EH, 07H, 00H, 00H
+        db 3CH, 12H, 00H, 00H, 2FH, 1CH, 00H, 00H
+        db 2EH, 08H, 00H, 00H, 78H, 0FEH, 0FFH, 0FFH
+        db 54H, 08H, 00H, 00H, 95H, 15H, 00H, 00H
+        db 0D7H, 01H, 00H, 00H, 0D6H, 0F5H, 0FFH, 0FFH
+        db 0C9H, 0FEH, 0FFH, 0FFH, 05H, 0F7H, 0FFH, 0FFH
+        db 8BH, 03H, 00H, 00H, 0FDH, 0EFH, 0FFH, 0FFH
+        db 8BH, 03H, 00H, 00H, 0AAH, 06H, 00H, 00H
+        db 74H, 03H, 00H, 00H, 7DH, 08H, 00H, 00H
+        db 7AH, 0F1H, 0FFH, 0FFH, 85H, 0FFH, 0FFH, 0FFH
+        db 6AH, 13H, 00H, 00H, 6AH, 13H, 00H, 00H
+        db 5AH, 10H, 00H, 00H, 26H, 0FBH, 0FFH, 0FFH
+        db 5AH, 10H, 00H, 00H, 0DBH, 05H, 00H, 00H
+        db 1BH, 0FAH, 0FFH, 0FFH, 58H, 02H, 00H, 00H
+        db 03H, 0BH, 00H, 00H, 03H, 0BH, 00H, 00H
+        db 1FH, 0FH, 00H, 00H, 0E4H, 0FCH, 0FFH, 0FFH
+        db 0B4H, 0FEH, 0FFH, 0FFH, 0F3H, 0BH, 00H, 00H
+        db 79H, 01H, 00H, 00H, 00H, 00H, 00H, 00H
+        db 28H, 09H, 00H, 00H, 9AH, 0F7H, 0FFH, 0FFH
+        db 28H, 09H, 00H, 00H, 28H, 09H, 00H, 00H
+        db 00H, 00H, 00H, 00H, 0B2H, 0F7H, 0FFH, 0FFH
+        db 0B0H, 0FCH, 0FFH, 0FFH, 90H, 00H, 00H, 00H
+        db 6FH, 00H, 00H, 00H, 13H, 0AH, 00H, 00H
+        db 13H, 0AH, 00H, 00H, 05H, 05H, 00H, 00H
+        db 37H, 08H, 00H, 00H, 48H, 11H, 00H, 00H
+        db 32H, 0F5H, 0FFH, 0FFH, 0D2H, 0F7H, 0FFH, 0FFH
+        db 11H, 0EH, 00H, 00H, 5DH, 0EEH, 0FFH, 0FFH
+        db 53H, 1CH, 00H, 00H, 53H, 1CH, 00H, 00H
+        db 72H, 0BH, 00H, 00H, 5DH, 18H, 00H, 00H
+        db 4AH, 08H, 00H, 00H, 7AH, 04H, 00H, 00H
+        db 4AH, 08H, 00H, 00H, 0AEH, 0BH, 00H, 00H
+        db 3FH, 0F3H, 0FFH, 0FFH, 9CH, 05H, 00H, 00H
+        db 94H, 0FFH, 0FFH, 0FFH, 0D8H, 03H, 00H, 00H
+        db 0AH, 00H, 00H, 00H, 0D8H, 03H, 00H, 00H
+        db 0A4H, 0FEH, 0FFH, 0FFH, 0FAH, 0F7H, 0FFH, 0FFH
+        db 7DH, 0E6H, 0FFH, 0FFH, 0A4H, 0FEH, 0FFH, 0FFH
+        db 0A4H, 0FEH, 0FFH, 0FFH, 8FH, 0FBH, 0FFH, 0FFH
+        db 9EH, 0FCH, 0FFH, 0FFH, 0DH, 08H, 00H, 00H
+        db 07H, 0EEH, 0FFH, 0FFH, 3FH, 0CH, 00H, 00H
+        db 96H, 0AH, 00H, 00H, 59H, 03H, 00H, 00H
+        db 96H, 0AH, 00H, 00H, 00H, 00H, 00H, 00H
+        db 0A0H, 08H, 00H, 00H, 0FFH, 0FAH, 0FFH, 0FFH
+        db 44H, 06H, 00H, 00H, 27H, 0F9H, 0FFH, 0FFH
+        db 0C2H, 04H, 00H, 00H, 66H, 07H, 00H, 00H
+        db 0E5H, 0FFH, 0FFH, 0FFH, 55H, 05H, 00H, 00H
+        db 0E7H, 00H, 00H, 00H, 00H, 00H, 00H, 00H
+        db 26H, 03H, 00H, 00H, 91H, 0FCH, 0FFH, 0FFH
+        db 26H, 03H, 00H, 00H, 3CH, 0FFH, 0FFH, 0FFH
+        db 3CH, 0FFH, 0FFH, 0FFH, 0CEH, 0F8H, 0FFH, 0FFH
+        db 80H, 02H, 00H, 00H, 69H, 0BH, 00H, 00H
+        db 0B3H, 0BH, 00H, 00H, 7EH, 01H, 00H, 00H
+        db 8CH, 04H, 00H, 00H, 0D3H, 01H, 00H, 00H
+        db 48H, 07H, 00H, 00H, 0F2H, 05H, 00H, 00H
+        db 0B0H, 11H, 00H, 00H, 9BH, 0F5H, 0FFH, 0FFH
+        db 2EH, 00H, 00H, 00H, 0D8H, 0BH, 00H, 00H
+        db 0DFH, 0FFH, 0FFH, 0FFH, 0F3H, 0F6H, 0FFH, 0FFH
+        db 1AH, 05H, 00H, 00H, 5BH, 04H, 00H, 00H
+        db 0FDH, 0F8H, 0FFH, 0FFH, 61H, 0FCH, 0FFH, 0FFH
+        db 61H, 0F7H, 0FFH, 0FFH, 0B4H, 06H, 00H, 00H
+        db 00H, 00H, 00H, 00H, 0B4H, 06H, 00H, 00H
+        db 3AH, 1CH, 00H, 00H, 0BCH, 02H, 00H, 00H
+        db 0ACH, 0FDH, 0FFH, 0FFH, 7FH, 09H, 00H, 00H
+        db 18H, 0FEH, 0FFH, 0FFH, 0BBH, 12H, 00H, 00H
+        db 0BBH, 12H, 00H, 00H, 0BBH, 12H, 00H, 00H
+        db 0BBH, 12H, 00H, 00H, 2FH, 01H, 00H, 00H
+        db 0BBH, 12H, 00H, 00H, 23H, 04H, 00H, 00H
+        db 45H, 05H, 00H, 00H, 45H, 05H, 00H, 00H
+        db 45H, 05H, 00H, 00H, 47H, 0FBH, 0FFH, 0FFH
+        db 3BH, 0BH, 00H, 00H, 0AAH, 07H, 00H, 00H
+        db 01H, 0FH, 00H, 00H, 83H, 07H, 00H, 00H
+        db 43H, 19H, 00H, 00H, 0E6H, 09H, 00H, 00H
+        db 0C1H, 0F4H, 0FFH, 0FFH, 0E6H, 09H, 00H, 00H
+        db 7AH, 00H, 00H, 00H, 3DH, 01H, 00H, 00H
+        db 0E4H, 0FCH, 0FFH, 0FFH, 49H, 0FAH, 0FFH, 0FFH
+        db 0EAH, 04H, 00H, 00H, 0EAH, 04H, 00H, 00H
+        db 0EAH, 04H, 00H, 00H, 98H, 00H, 00H, 00H
+        db 20H, 12H, 00H, 00H, 3EH, 10H, 00H, 00H
+        db 00H, 0AH, 00H, 00H, 0B3H, 0FFH, 0FFH, 0FFH
+        db 52H, 0FBH, 0FFH, 0FFH, 0E0H, 02H, 00H, 00H
+        db 0E2H, 01H, 00H, 00H, 08H, 02H, 00H, 00H
+        db 24H, 0EH, 00H, 00H, 59H, 00H, 00H, 00H
+        db 0DAH, 0EH, 00H, 00H, 0FEH, 10H, 00H, 00H
+        db 92H, 0BH, 00H, 00H, 0FEH, 10H, 00H, 00H
+        db 0B6H, 16H, 00H, 00H, 93H, 0F9H, 0FFH, 0FFH
+        db 3CH, 0DH, 00H, 00H, 5FH, 0DH, 00H, 00H
+        db 88H, 03H, 00H, 00H, 0D4H, 0F9H, 0FFH, 0FFH
+        db 0C9H, 03H, 00H, 00H, 0C9H, 03H, 00H, 00H
+        db 17H, 0FEH, 0FFH, 0FFH, 39H, 0FFH, 0FFH, 0FFH
+        db 95H, 13H, 00H, 00H, 0A2H, 0AH, 00H, 00H
+        db 0CFH, 04H, 00H, 00H, 0DFH, 29H, 00H, 00H
+        db 0FDH, 00H, 00H, 00H, 34H, 10H, 00H, 00H
+        db 89H, 0DH, 00H, 00H, 3CH, 08H, 00H, 00H
+        db 0CCH, 13H, 00H, 00H, 35H, 15H, 00H, 00H
+        db 62H, 16H, 00H, 00H, 86H, 08H, 00H, 00H
+        db 0FH, 00H, 00H, 00H, 00H, 00H, 00H, 00H
+        db 3AH, 02H, 00H, 00H, 0F9H, 11H, 00H, 00H
+        db 5CH, 00H, 00H, 00H, 0C5H, 12H, 00H, 00H
+        db 50H, 06H, 00H, 00H, 53H, 01H, 00H, 00H
+        db 03H, 04H, 00H, 00H, 21H, 03H, 00H, 00H
+        db 30H, 06H, 00H, 00H, 9CH, 12H, 00H, 00H
+        db 9CH, 12H, 00H, 00H, 0E9H, 0BH, 00H, 00H
+        db 48H, 03H, 00H, 00H, 0DH, 24H, 00H, 00H
+        db 03H, 1BH, 00H, 00H, 19H, 0BH, 00H, 00H
+        db 0C6H, 01H, 00H, 00H, 00H, 00H, 00H, 00H
+        db 64H, 0EH, 00H, 00H, 0D9H, 0FDH, 0FFH, 0FFH
+        db 0ADH, 04H, 00H, 00H, 0D5H, 0DH, 00H, 00H
+        db 2FH, 0F9H, 0FFH, 0FFH, 0B5H, 09H, 00H, 00H
+        db 0B5H, 09H, 00H, 00H, 0DAH, 0E9H, 0FFH, 0FFH
+        db 03H, 0F8H, 0FFH, 0FFH, 0F0H, 0F3H, 0FFH, 0FFH
+        db 6CH, 09H, 00H, 00H, 6CH, 09H, 00H, 00H
+        db 99H, 05H, 00H, 00H, 9CH, 0F8H, 0FFH, 0FFH
+        db 0A2H, 0E5H, 0FFH, 0FFH, 00H, 00H, 00H, 00H
+        db 43H, 01H, 00H, 00H, 97H, 0EH, 00H, 00H
+        db 16H, 0FEH, 0FFH, 0FFH, 85H, 0DH, 00H, 00H
+        db 5FH, 0F2H, 0FFH, 0FFH, 0CEH, 0F4H, 0FFH, 0FFH
+        db 00H, 00H, 00H, 00H, 1BH, 0BH, 00H, 00H
+        db 02H, 0F9H, 0FFH, 0FFH, 1BH, 0BH, 00H, 00H
+        db 00H, 00H, 00H, 00H, 1BH, 0BH, 00H, 00H
+        db 0EBH, 0FFH, 0FFH, 0FFH, 2EH, 04H, 00H, 00H
+        db 24H, 0FBH, 0FFH, 0FFH, 0C9H, 08H, 00H, 00H
+        db 0AEH, 0EEH, 0FFH, 0FFH, 0BAH, 06H, 00H, 00H
+        db 0BAH, 06H, 00H, 00H, 19H, 0EH, 00H, 00H
+        db 45H, 0F9H, 0FFH, 0FFH, 0E3H, 09H, 00H, 00H
+        db 11H, 0FDH, 0FFH, 0FFH, 0F2H, 1CH, 00H, 00H
+        db 06H, 05H, 00H, 00H, 59H, 0F5H, 0FFH, 0FFH
+        db 71H, 09H, 00H, 00H, 11H, 16H, 00H, 00H
+        db 0A4H, 03H, 00H, 00H, 0BDH, 0FH, 00H, 00H
+        db 0CEH, 0F6H, 0FFH, 0FFH, 0BCH, 0EH, 00H, 00H
+        db 8AH, 02H, 00H, 00H, 8CH, 0FCH, 0FFH, 0FFH
+        db 1EH, 02H, 00H, 00H, 87H, 12H, 00H, 00H
+        db 7CH, 17H, 00H, 00H, 7CH, 17H, 00H, 00H
+        db 0EBH, 05H, 00H, 00H, 98H, 00H, 00H, 00H
+        db 5FH, 0FFH, 0FFH, 0FFH, 90H, 0BH, 00H, 00H
+        db 91H, 0EH, 00H, 00H, 48H, 13H, 00H, 00H
+        db 90H, 02H, 00H, 00H, 73H, 14H, 00H, 00H
+        db 73H, 14H, 00H, 00H, 73H, 14H, 00H, 00H
+        db 0F9H, 02H, 00H, 00H, 01H, 1CH, 00H, 00H
+        db 90H, 02H, 00H, 00H, 0FAH, 0FEH, 0FFH, 0FFH
+        db 62H, 03H, 00H, 00H, 5FH, 03H, 00H, 00H
+        db 0D6H, 01H, 00H, 00H, 0E5H, 09H, 00H, 00H
+        db 0C1H, 0FAH, 0FFH, 0FFH, 0F3H, 06H, 00H, 00H
+        db 66H, 06H, 00H, 00H, 66H, 06H, 00H, 00H
+        db 0E4H, 0FDH, 0FFH, 0FFH, 0A8H, 17H, 00H, 00H
+        db 3FH, 0FFH, 0FFH, 0FFH, 4DH, 15H, 00H, 00H
+        db 4AH, 0EH, 00H, 00H, 7DH, 0AH, 00H, 00H
+        db 7DH, 0AH, 00H, 00H, 50H, 08H, 00H, 00H
+        db 50H, 08H, 00H, 00H, 0D9H, 0FEH, 0FFH, 0FFH
+        db 0F2H, 0BH, 00H, 00H, 8AH, 0F9H, 0FFH, 0FFH
+        db 50H, 08H, 00H, 00H, 32H, 01H, 00H, 00H
+        db 0B3H, 0FDH, 0FFH, 0FFH, 04H, 11H, 00H, 00H
+        db 9DH, 0FDH, 0FFH, 0FFH, 96H, 0F4H, 0FFH, 0FFH
+        db 16H, 01H, 00H, 00H, 0D3H, 0F8H, 0FFH, 0FFH
+        db 00H, 00H, 00H, 00H, 0D0H, 07H, 00H, 00H
+        db 02H, 03H, 00H, 00H, 0D0H, 07H, 00H, 00H
+        db 0D0H, 07H, 00H, 00H, 19H, 0CH, 00H, 00H
+        db 19H, 0CH, 00H, 00H, 19H, 0CH, 00H, 00H
+        db 89H, 0AH, 00H, 00H, 7DH, 07H, 00H, 00H
+        db 25H, 09H, 00H, 00H, 0BEH, 0FH, 00H, 00H
+        db 0BEH, 0FH, 00H, 00H, 0C3H, 18H, 00H, 00H
+
+
+SECTION .bss    
+
+
+SECTION .rodata 
+
+L_003:
+        db 25H, 64H, 0AH, 00H
+
+
+SECTION .eh_frame align=8
+
+        db 14H, 00H, 00H, 00H, 00H, 00H, 00H, 00H
+        db 01H, 7AH, 52H, 00H, 01H, 78H, 10H, 01H
+        db 1BH, 0CH, 07H, 08H, 90H, 01H, 00H, 00H
+        db 1CH, 00H, 00H, 00H, 1CH, 00H, 00H, 00H
+        dd main-$-20H
+        dd 00000055H, 100E4100H
+        dd 0D430286H, 0C500206H
+        dd 00000807H
+
+
---
+++ lib/output.s
@@ -0,0 +1,653 @@
+	.file	"output.cpp"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.type	_ZStL13allocator_arg, @object
+	.size	_ZStL13allocator_arg, 1
+_ZStL13allocator_arg:
+	.zero	1
+	.type	_ZStL6ignore, @object
+	.size	_ZStL6ignore, 1
+_ZStL6ignore:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.align 4
+	.type	_ZN9__gnu_cxxL21__default_lock_policyE, @object
+	.size	_ZN9__gnu_cxxL21__default_lock_policyE, 4
+_ZN9__gnu_cxxL21__default_lock_policyE:
+	.long	2
+	.type	_ZStL10defer_lock, @object
+	.size	_ZStL10defer_lock, 1
+_ZStL10defer_lock:
+	.zero	1
+	.type	_ZStL11try_to_lock, @object
+	.size	_ZStL11try_to_lock, 1
+_ZStL11try_to_lock:
+	.zero	1
+	.type	_ZStL10adopt_lock, @object
+	.size	_ZStL10adopt_lock, 1
+_ZStL10adopt_lock:
+	.zero	1
+	.align 4
+	.type	_ZNSt15regex_constantsL5icaseE, @object
+	.size	_ZNSt15regex_constantsL5icaseE, 4
+_ZNSt15regex_constantsL5icaseE:
+	.long	1
+	.align 4
+	.type	_ZNSt15regex_constantsL6nosubsE, @object
+	.size	_ZNSt15regex_constantsL6nosubsE, 4
+_ZNSt15regex_constantsL6nosubsE:
+	.long	2
+	.align 4
+	.type	_ZNSt15regex_constantsL8optimizeE, @object
+	.size	_ZNSt15regex_constantsL8optimizeE, 4
+_ZNSt15regex_constantsL8optimizeE:
+	.long	4
+	.align 4
+	.type	_ZNSt15regex_constantsL7collateE, @object
+	.size	_ZNSt15regex_constantsL7collateE, 4
+_ZNSt15regex_constantsL7collateE:
+	.long	8
+	.align 4
+	.type	_ZNSt15regex_constantsL10ECMAScriptE, @object
+	.size	_ZNSt15regex_constantsL10ECMAScriptE, 4
+_ZNSt15regex_constantsL10ECMAScriptE:
+	.long	16
+	.align 4
+	.type	_ZNSt15regex_constantsL5basicE, @object
+	.size	_ZNSt15regex_constantsL5basicE, 4
+_ZNSt15regex_constantsL5basicE:
+	.long	32
+	.align 4
+	.type	_ZNSt15regex_constantsL8extendedE, @object
+	.size	_ZNSt15regex_constantsL8extendedE, 4
+_ZNSt15regex_constantsL8extendedE:
+	.long	64
+	.align 4
+	.type	_ZNSt15regex_constantsL3awkE, @object
+	.size	_ZNSt15regex_constantsL3awkE, 4
+_ZNSt15regex_constantsL3awkE:
+	.long	128
+	.align 4
+	.type	_ZNSt15regex_constantsL4grepE, @object
+	.size	_ZNSt15regex_constantsL4grepE, 4
+_ZNSt15regex_constantsL4grepE:
+	.long	256
+	.align 4
+	.type	_ZNSt15regex_constantsL5egrepE, @object
+	.size	_ZNSt15regex_constantsL5egrepE, 4
+_ZNSt15regex_constantsL5egrepE:
+	.long	512
+	.align 4
+	.type	_ZNSt15regex_constantsL12__polynomialE, @object
+	.size	_ZNSt15regex_constantsL12__polynomialE, 4
+_ZNSt15regex_constantsL12__polynomialE:
+	.long	1024
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_defaultE, @object
+	.size	_ZNSt15regex_constantsL13match_defaultE, 4
+_ZNSt15regex_constantsL13match_defaultE:
+	.zero	4
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_bolE, @object
+	.size	_ZNSt15regex_constantsL13match_not_bolE, 4
+_ZNSt15regex_constantsL13match_not_bolE:
+	.long	1
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_eolE, @object
+	.size	_ZNSt15regex_constantsL13match_not_eolE, 4
+_ZNSt15regex_constantsL13match_not_eolE:
+	.long	2
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_bowE, @object
+	.size	_ZNSt15regex_constantsL13match_not_bowE, 4
+_ZNSt15regex_constantsL13match_not_bowE:
+	.long	4
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_eowE, @object
+	.size	_ZNSt15regex_constantsL13match_not_eowE, 4
+_ZNSt15regex_constantsL13match_not_eowE:
+	.long	8
+	.align 4
+	.type	_ZNSt15regex_constantsL9match_anyE, @object
+	.size	_ZNSt15regex_constantsL9match_anyE, 4
+_ZNSt15regex_constantsL9match_anyE:
+	.long	16
+	.align 4
+	.type	_ZNSt15regex_constantsL14match_not_nullE, @object
+	.size	_ZNSt15regex_constantsL14match_not_nullE, 4
+_ZNSt15regex_constantsL14match_not_nullE:
+	.long	32
+	.align 4
+	.type	_ZNSt15regex_constantsL16match_continuousE, @object
+	.size	_ZNSt15regex_constantsL16match_continuousE, 4
+_ZNSt15regex_constantsL16match_continuousE:
+	.long	64
+	.align 4
+	.type	_ZNSt15regex_constantsL16match_prev_availE, @object
+	.size	_ZNSt15regex_constantsL16match_prev_availE, 4
+_ZNSt15regex_constantsL16match_prev_availE:
+	.long	128
+	.align 4
+	.type	_ZNSt15regex_constantsL14format_defaultE, @object
+	.size	_ZNSt15regex_constantsL14format_defaultE, 4
+_ZNSt15regex_constantsL14format_defaultE:
+	.zero	4
+	.align 4
+	.type	_ZNSt15regex_constantsL10format_sedE, @object
+	.size	_ZNSt15regex_constantsL10format_sedE, 4
+_ZNSt15regex_constantsL10format_sedE:
+	.long	256
+	.align 4
+	.type	_ZNSt15regex_constantsL14format_no_copyE, @object
+	.size	_ZNSt15regex_constantsL14format_no_copyE, 4
+_ZNSt15regex_constantsL14format_no_copyE:
+	.long	512
+	.align 4
+	.type	_ZNSt15regex_constantsL17format_first_onlyE, @object
+	.size	_ZNSt15regex_constantsL17format_first_onlyE, 4
+_ZNSt15regex_constantsL17format_first_onlyE:
+	.long	1024
+	.align 4
+	.type	_ZNSt15regex_constantsL13error_collateE, @object
+	.size	_ZNSt15regex_constantsL13error_collateE, 4
+_ZNSt15regex_constantsL13error_collateE:
+	.zero	4
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_ctypeE, @object
+	.size	_ZNSt15regex_constantsL11error_ctypeE, 4
+_ZNSt15regex_constantsL11error_ctypeE:
+	.long	1
+	.align 4
+	.type	_ZNSt15regex_constantsL12error_escapeE, @object
+	.size	_ZNSt15regex_constantsL12error_escapeE, 4
+_ZNSt15regex_constantsL12error_escapeE:
+	.long	2
+	.align 4
+	.type	_ZNSt15regex_constantsL13error_backrefE, @object
+	.size	_ZNSt15regex_constantsL13error_backrefE, 4
+_ZNSt15regex_constantsL13error_backrefE:
+	.long	3
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_brackE, @object
+	.size	_ZNSt15regex_constantsL11error_brackE, 4
+_ZNSt15regex_constantsL11error_brackE:
+	.long	4
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_parenE, @object
+	.size	_ZNSt15regex_constantsL11error_parenE, 4
+_ZNSt15regex_constantsL11error_parenE:
+	.long	5
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_braceE, @object
+	.size	_ZNSt15regex_constantsL11error_braceE, 4
+_ZNSt15regex_constantsL11error_braceE:
+	.long	6
+	.align 4
+	.type	_ZNSt15regex_constantsL14error_badbraceE, @object
+	.size	_ZNSt15regex_constantsL14error_badbraceE, 4
+_ZNSt15regex_constantsL14error_badbraceE:
+	.long	7
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_rangeE, @object
+	.size	_ZNSt15regex_constantsL11error_rangeE, 4
+_ZNSt15regex_constantsL11error_rangeE:
+	.long	8
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_spaceE, @object
+	.size	_ZNSt15regex_constantsL11error_spaceE, 4
+_ZNSt15regex_constantsL11error_spaceE:
+	.long	9
+	.align 4
+	.type	_ZNSt15regex_constantsL15error_badrepeatE, @object
+	.size	_ZNSt15regex_constantsL15error_badrepeatE, 4
+_ZNSt15regex_constantsL15error_badrepeatE:
+	.long	10
+	.align 4
+	.type	_ZNSt15regex_constantsL16error_complexityE, @object
+	.size	_ZNSt15regex_constantsL16error_complexityE, 4
+_ZNSt15regex_constantsL16error_complexityE:
+	.long	11
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_stackE, @object
+	.size	_ZNSt15regex_constantsL11error_stackE, 4
+_ZNSt15regex_constantsL11error_stackE:
+	.long	12
+	.align 8
+	.type	_ZNSt8__detailL19_S_invalid_state_idE, @object
+	.size	_ZNSt8__detailL19_S_invalid_state_idE, 8
+_ZNSt8__detailL19_S_invalid_state_idE:
+	.quad	-1
+	.globl	ans
+	.data
+	.align 32
+	.type	ans, @object
+	.size	ans, 1312
+ans:
+	.long	-2262
+	.long	-2262
+	.long	1132
+	.long	932
+	.long	1016
+	.long	4035
+	.long	2651
+	.long	6518
+	.long	3952
+	.long	1335
+	.long	1535
+	.long	1838
+	.long	4668
+	.long	7215
+	.long	2094
+	.long	-392
+	.long	2132
+	.long	5525
+	.long	471
+	.long	-2602
+	.long	-311
+	.long	-2299
+	.long	907
+	.long	-4099
+	.long	907
+	.long	1706
+	.long	884
+	.long	2173
+	.long	-3718
+	.long	-123
+	.long	4970
+	.long	4970
+	.long	4186
+	.long	-1242
+	.long	4186
+	.long	1499
+	.long	-1509
+	.long	600
+	.long	2819
+	.long	2819
+	.long	3871
+	.long	-796
+	.long	-332
+	.long	3059
+	.long	377
+	.long	0
+	.long	2344
+	.long	-2150
+	.long	2344
+	.long	2344
+	.long	0
+	.long	-2126
+	.long	-848
+	.long	144
+	.long	111
+	.long	2579
+	.long	2579
+	.long	1285
+	.long	2103
+	.long	4424
+	.long	-2766
+	.long	-2094
+	.long	3601
+	.long	-4515
+	.long	7251
+	.long	7251
+	.long	2930
+	.long	6237
+	.long	2122
+	.long	1146
+	.long	2122
+	.long	2990
+	.long	-3265
+	.long	1436
+	.long	-108
+	.long	984
+	.long	10
+	.long	984
+	.long	-348
+	.long	-2054
+	.long	-6531
+	.long	-348
+	.long	-348
+	.long	-1137
+	.long	-866
+	.long	2061
+	.long	-4601
+	.long	3135
+	.long	2710
+	.long	857
+	.long	2710
+	.long	0
+	.long	2208
+	.long	-1281
+	.long	1604
+	.long	-1753
+	.long	1218
+	.long	1894
+	.long	-27
+	.long	1365
+	.long	231
+	.long	0
+	.long	806
+	.long	-879
+	.long	806
+	.long	-196
+	.long	-196
+	.long	-1842
+	.long	640
+	.long	2921
+	.long	2995
+	.long	382
+	.long	1164
+	.long	467
+	.long	1864
+	.long	1522
+	.long	4528
+	.long	-2661
+	.long	46
+	.long	3032
+	.long	-33
+	.long	-2317
+	.long	1306
+	.long	1115
+	.long	-1795
+	.long	-927
+	.long	-2207
+	.long	1716
+	.long	0
+	.long	1716
+	.long	7226
+	.long	700
+	.long	-596
+	.long	2431
+	.long	-488
+	.long	4795
+	.long	4795
+	.long	4795
+	.long	4795
+	.long	303
+	.long	4795
+	.long	1059
+	.long	1349
+	.long	1349
+	.long	1349
+	.long	-1209
+	.long	2875
+	.long	1962
+	.long	3841
+	.long	1923
+	.long	6467
+	.long	2534
+	.long	-2879
+	.long	2534
+	.long	122
+	.long	317
+	.long	-796
+	.long	-1463
+	.long	1258
+	.long	1258
+	.long	1258
+	.long	152
+	.long	4640
+	.long	4158
+	.long	2560
+	.long	-77
+	.long	-1198
+	.long	736
+	.long	482
+	.long	520
+	.long	3620
+	.long	89
+	.long	3802
+	.long	4350
+	.long	2962
+	.long	4350
+	.long	5814
+	.long	-1645
+	.long	3388
+	.long	3423
+	.long	904
+	.long	-1580
+	.long	969
+	.long	969
+	.long	-489
+	.long	-199
+	.long	5013
+	.long	2722
+	.long	1231
+	.long	10719
+	.long	253
+	.long	4148
+	.long	3465
+	.long	2108
+	.long	5068
+	.long	5429
+	.long	5730
+	.long	2182
+	.long	15
+	.long	0
+	.long	570
+	.long	4601
+	.long	92
+	.long	4805
+	.long	1616
+	.long	339
+	.long	1027
+	.long	801
+	.long	1584
+	.long	4764
+	.long	4764
+	.long	3049
+	.long	840
+	.long	9229
+	.long	6915
+	.long	2841
+	.long	454
+	.long	0
+	.long	3684
+	.long	-551
+	.long	1197
+	.long	3541
+	.long	-1745
+	.long	2485
+	.long	2485
+	.long	-5670
+	.long	-2045
+	.long	-3088
+	.long	2412
+	.long	2412
+	.long	1433
+	.long	-1892
+	.long	-6750
+	.long	0
+	.long	323
+	.long	3735
+	.long	-490
+	.long	3461
+	.long	-3489
+	.long	-2866
+	.long	0
+	.long	2843
+	.long	-1790
+	.long	2843
+	.long	0
+	.long	2843
+	.long	-21
+	.long	1070
+	.long	-1244
+	.long	2249
+	.long	-4434
+	.long	1722
+	.long	1722
+	.long	3609
+	.long	-1723
+	.long	2531
+	.long	-751
+	.long	7410
+	.long	1286
+	.long	-2727
+	.long	2417
+	.long	5649
+	.long	932
+	.long	4029
+	.long	-2354
+	.long	3772
+	.long	650
+	.long	-884
+	.long	542
+	.long	4743
+	.long	6012
+	.long	6012
+	.long	1515
+	.long	152
+	.long	-161
+	.long	2960
+	.long	3729
+	.long	4936
+	.long	656
+	.long	5235
+	.long	5235
+	.long	5235
+	.long	761
+	.long	7169
+	.long	656
+	.long	-262
+	.long	866
+	.long	863
+	.long	470
+	.long	2533
+	.long	-1343
+	.long	1779
+	.long	1638
+	.long	1638
+	.long	-540
+	.long	6056
+	.long	-193
+	.long	5453
+	.long	3658
+	.long	2685
+	.long	2685
+	.long	2128
+	.long	2128
+	.long	-295
+	.long	3058
+	.long	-1654
+	.long	2128
+	.long	306
+	.long	-589
+	.long	4356
+	.long	-611
+	.long	-2922
+	.long	278
+	.long	-1837
+	.long	0
+	.long	2000
+	.long	770
+	.long	2000
+	.long	2000
+	.long	3097
+	.long	3097
+	.long	3097
+	.long	2697
+	.long	1917
+	.long	2341
+	.long	4030
+	.long	4030
+	.long	6339
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB8188:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$328, -4(%rbp)
+	movl	$0, -8(%rbp)
+.L3:
+	movl	-8(%rbp), %eax
+	cmpl	-4(%rbp), %eax
+	jge	.L2
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	leaq	ans(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	addl	$1, -8(%rbp)
+	jmp	.L3
+.L2:
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE8188:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB9495:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L7
+	cmpl	$65535, -8(%rbp)
+	jne	.L7
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L7:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE9495:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_ans, @function
+_GLOBAL__sub_I_ans:
+.LFB9518:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE9518:
+	.size	_GLOBAL__sub_I_ans, .-_GLOBAL__sub_I_ans
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_ans
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
